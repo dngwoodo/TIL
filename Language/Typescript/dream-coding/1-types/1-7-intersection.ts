f460877c1e5f9d27930e33ad44aae4f11d4ec747
dngwoodo <dngwoodo@gmail.com>
2021-01-22 23:07:17 +0900

    Typescript/dream-coding: 2.12 Intersection 타입 ✨

---
+++ Language/Typescript/dream-coding/1-types/1-7-intersection.ts
@@ -0,0 +1,26 @@
+{
+    /**
+     * Intersection Types: &
+     */
+    type Student = {
+        name : string;
+        score: number;
+    }
+
+    type Worker = {
+        employeeId: number;
+        work: () => void;
+    }
+
+    function internWork(person: Student & Worker){
+        // 합집합을 의미함.
+        console.log(person.name, person.employeeId, person.score, person.work());
+    }
+
+    internWork({
+        name: 'ellie',
+        score: 1,
+        employeeId: 123,
+        work: () => {}
+    })
+}
