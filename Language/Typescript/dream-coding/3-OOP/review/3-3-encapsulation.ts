e48f8a17304e206a090054943990b9af1f4f8518
dngwoodo <dngwoodo@gmail.com>
2021-01-26 22:21:37 +0900

    Typescript/dream-coding/review: 3-3-encapsulation.ts 비교

--- Language/Typescript/dream-coding/3-OOP/review/3-3-encapsulation.ts
+++ Language/Typescript/dream-coding/3-OOP/review/3-3-encapsulation.ts
@@ -19,4 +19,44 @@ class CoffeeMachine {
         }
         this.coffeeBeansGram -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
     }
+
+    // private는 외부에서 접근이 불가능하므로 fillCoffeeBeans 메서드를 활용하여 변경해준다.
+    fillCoffeeBeans(beans: number) {
+        if(beans < 0) {
+            throw new Error('value for beans should be greater than 0');
+        }
+        this.coffeeBeansGram += beans
+    }
 }
+
+// getter, setter ✨
+class User {
+    constructor(private firstName: string, private lastName: string) {};
+
+    fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+    
+    get getFirstName(): string {
+        return this.firstName;
+    }
+
+    set setFirstName(newFirstName) {
+        if(newFirstName.length === 1) {
+            throw new Error('성은 한 글자보다 더 길어야 합니다.');
+        }
+        this.firstName = newFirstName
+    }
+}
+
+const user = new User('kim', 'dongwoo');
+console.log(user.fullName()); // kim dongwoo
+
+// user.firstName; // Property 'firstName' is private and only accessible within class 'User'.ts(2341)
+console.log(user.getFirstName); // kim
+
+// user.setFirstName = 'l'; // throw new Error('성은 한 글자보다 더 길어야 합니다.');
+user.setFirstName = 'lee';
+console.log(user.getFirstName); // lee
+
+console.log(user.fullName()); // lee dongwoo
