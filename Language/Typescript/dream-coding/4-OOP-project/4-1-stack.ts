3ac994770ac3c0cb25f39a33a1950d7f8e315bdd
dngwoodo <dngwoodo@gmail.com>
2021-01-28 17:18:37 +0900

    Typescript/dream-coding: 5.1 객체지향 챌린지 소개 ❓

---
+++ Language/Typescript/dream-coding/4-OOP-project/4-1-stack.ts
@@ -0,0 +1,31 @@
+// stack의 기본 기능
+// LIFO (Last In First Out)
+// push
+// pop
+
+class Stack {
+    arr:any[] = []
+    push(value: any){
+        this.arr = [...this.arr, value]
+        return this.arr
+    }
+
+    pop(){
+        if(this.arr.length === 0) {
+            throw new Error("pop할 요소가 없습니다.");
+        }
+        this.arr = this.arr.slice(0, -1)
+        return this.arr
+    }
+}
+
+const a = new Stack()
+console.log(a.push(1))
+console.log(a.push(1))
+console.log(a.push(1))
+console.log(a.push(23030))
+console.log(a.pop())
+console.log(a.pop())
+console.log(a.pop())
+console.log(a.pop())
+console.log(a.pop()) // error
