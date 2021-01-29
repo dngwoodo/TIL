b643d74707f4aacec55bc111bfde1f072ec3b5b7
dngwoodo <dngwoodo@gmail.com>
2021-01-29 17:08:15 +0900

    Typescript/dream-coding: 5.2 객체지향 챌린지 힌트 😜를 보고 재코딩

--- Language/Typescript/dream-coding/4-OOP-project/4-2-stack.ts
+++ Language/Typescript/dream-coding/4-OOP-project/4-2-stack.ts
@@ -24,3 +24,60 @@ interface Stack {
 // 단일 연결 리스트를 수직으로 세우면 stack이 된다.
 // Head가 가르키는 노드 위에는 아무것도 없어야 한다.
 // Stack에 노드가 하나도 없다면 Head는 null을 가르킨다.
+
+// 힌트를 듣고 코딩한 것
+type Head = null | {value: string, head: Head}
+class DongwooStack implements Stack {
+    head: null | Head;
+    stackSize: number = 0;
+    constructor(readonly size: number) {}
+
+    push(value: string): void {
+        this.stackSize += 1;
+        if(this.stackSize > this.size) throw new Error("스택의 크기를 초과했습니다.");
+        else {
+            const obj = { value, head: this.head };
+            this.head = obj;
+            console.log(this.stackSize, this.size);
+            
+            // console.log(this.head);
+        }
+    }
+
+    pop(): string {
+        if(this.head) {
+            const popValue = this.head.value
+            this.head = this.head.head;
+            this.stackSize -= 1;
+            console.log(this.head)
+            return popValue;
+        } else {
+            throw new Error("스택에 요소가 없습니다.");
+        }
+    }
+}
+
+const stack = new DongwooStack(10);
+stack.push("1");
+stack.push("2");
+stack.push("3");
+stack.push("4");
+stack.push("5");
+stack.push("6");
+stack.push("7");
+stack.push("8");
+stack.push("9");
+stack.push("10");
+// stack.push("11"); // throw new Error("스택의 크기를 초과했습니다.");
+
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+// stack.pop(); // throw new Error("스택에 요소가 없습니다.");
