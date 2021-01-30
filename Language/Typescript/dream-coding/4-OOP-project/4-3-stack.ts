1d1a97bf329fe79f0dd1ea44c2bdfcf9738b6ef2
dngwoodo <dngwoodo@gmail.com>
2021-01-30 11:14:59 +0900

    Typescript/dream-coding: 5.3 객치지향 솔루션 함께 구현하기 💡 - part3

--- Language/Typescript/dream-coding/4-OOP-project/4-3-stack.ts
+++ Language/Typescript/dream-coding/4-OOP-project/4-3-stack.ts
@@ -20,10 +20,24 @@ class StackImpl implements Stack{
     // 2. size는 사용자가 읽을 수 있어야 한다.
     // 3. 외부에서 쓰는 것과 내부에서 쓰는 것의 변수이름이 같으면 _를 붙여줘야 한다.
     private _size: number = 0; // 내부에서 쓰는 것
+
+    /**
+     * 5. 단일 연결 리스트에 필요한 head를 생성
+     * stack에 node가 없으면 head는 undefined이므로 옵셔널을 사용한다.
+     */
+    private head?: StackNode;
+
     get size() { // 외부에서 쓰는 것
         return this._size;
     }
     push(value: string ){
+        /**
+         * 6. node를 하나 생성하고 가장 마지막에 들어온 node(head)를 가르킨다.
+         * 그리고 head는 새로 들어온 node를 가르켜야 한다.
+         */
+        const node: StackNode = { value, next: this.head }
+        this.head = node;
+
         this._size++;
     }
     pop(): string {
