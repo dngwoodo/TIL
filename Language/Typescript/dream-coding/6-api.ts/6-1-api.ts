bce6f2de33e425997e015e7b0ddc025074f35e4f
dngwoodo <dngwoodo@gmail.com>
2021-02-01 20:56:17 +0900

    Typescript/dream-coding: 8.2 API 읽어 보기 💡 - part2

--- Language/Typescript/dream-coding/6-api.ts/6-1-api.ts
+++ Language/Typescript/dream-coding/6-api.ts/6-1-api.ts
@@ -24,15 +24,43 @@ const b = [1,'1', false]
 b.concat([3,4,5],4,5,6) // concat(...items: (T | ConcatArray<T>)[]): T[];
 b.concat([3,4,5]) // concat(...items: ConcatArray<T>[]): T[];
 
-// every 사용해보기
-// predicate는 시연을 하는 무언가로 해석하면 된다.
-// every(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean;
-// unknown이란 리턴되는 값이 무엇인지 알 수 없을 경우 적는다.
-// unknown이란 개발자에게 내부로직에서 타입을 좁혀서 지정해줘 라는 의미를 가진다. 
-// any와 다른점은 type assertion, type guard 등을 사용해서 타입을 지정해주고 사용해야 한다.
+
+/**
+ * every 사용해보기
+ * predicate는 시연을 하는 무언가로 해석하면 된다.
+ * every(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean;
+ * unknown이란 리턴되는 값이 무엇인지 알 수 없을 경우 적는다.
+ * unknown이란 개발자에게 내부로직에서 타입을 좁혀서 지정해줘 라는 의미를 가진다. 
+ * any와 다른점은 type assertion, type guard 등을 사용해서 타입을 지정해주고 사용해야 한다.
+ */
+ 
 type Student = {
     passed: boolean;
 }
 
 const students: Student[] = [{passed: true}, {passed: true}, {passed: false}];
 students.every(student => student.passed); // 하나라도 false가 뜨면 종료 후 false 반환
+
+/**
+ * every 사용해보기2
+ * every<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): this is S[];
+ * T 타입을 상속하는 S(서브)타입을 사용할 수 있다.
+ * value is S는 value가 S에 속하는지 보는 것이다. 즉 배열안에 있는 값이 S인지 보는 것
+ * is라는 것은 type predicate(user-defined type guards) 이라고 한다.
+ */
+
+ class Animal {}
+ class Cat extends Animal {
+     isCat: boolean = true;
+ }
+
+ class Dog extends Animal {
+     isDog: boolean = false;
+ }
+
+ const animals: Animal[] = [new Cat(), new Cat(), new Dog()];
+ function isCat(animal: Animal): animal is Cat {
+     return (animal as Cat).isCat !== undefined;
+ }
+
+ animals.every<Cat>(isCat);
