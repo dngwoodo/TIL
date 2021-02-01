405682e5353e22cd10f04896fd8b43aa7b26ad27
dngwoodo <dngwoodo@gmail.com>
2021-02-01 22:26:19 +0900

    Typescript/dream-coding: 9.4 우아하게 에러를 처리 하는 방법 - part2

--- Language/Typescript/dream-coding/7-exception/7-2-handling.ts
+++ Language/Typescript/dream-coding/7-exception/7-2-handling.ts
@@ -12,21 +12,32 @@ class UserService {
     constructor(private client: NetworkClient) {}
     
     login() {
-        this.client.tryConnect();
-        // login...
+            this.client.tryConnect();
+            // login...    
     }
 }
 
-// const client = new NetworkClient();
-// const service = new UserService(client);
-// service.login(); // throw new Error('no network');
+const client = new NetworkClient();
+const service = new UserService(client);
+service.login(); // catched!
 
 class App {
     constructor(private userService: UserService) {}
     run() {
-        this.userService.login();
+        try {
+            this.userService.login();    
+        } catch (error) {
+            // show dialog to user
+        }
+        
     }
 }
 
 const app = new App(service);
-app.run(); // // throw new Error('no network');
+app.run(); // catched!
+
+// 정리
+// 생각해야 되는 것은 내가 에러처리를 해주는 곳에서 의미있는 에러 처리를 할 수 있을지 생각해야 한다.
+// 기본적으로 UserService.login에서 try catch를 해줘야 될 것 같지만 저기서 에러처리를 해봤자 
+// 사용자에게 보여줄수있는 부분도 없고 무언가 반환해줄 값도 딱히 없기 때문에 안해주는게 더 낫다.
+// 그러므로 실제 어플리케이션단인 App 클래스에서 try catch를 이용하여 에러가 났을 경우 사용자에게 dialog를 보여주는 것이 낫다.
