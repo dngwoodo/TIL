6c970701af8a6cac856a85c48e765d6d454bb0f1
dngwoodo <dngwoodo@gmail.com>
2021-02-03 21:23:16 +0900

    Typescript/dream-coding: 10.6 Mapped Type - part2(mapped type 사용해보기)

--- Language/Typescript/dream-coding/8-advanced/8-3-map.ts
+++ Language/Typescript/dream-coding/8-advanced/8-3-map.ts
@@ -1,3 +1,4 @@
+export {}
 // 왜 사용하는가?
 // 이렇게 세가지 type이 있을 경우 Video에 description라는 프로퍼티가 추가되면 나머지 두 타입도 다 추가해줘야 된다.
 
@@ -7,14 +8,38 @@ type Video = {
     description: string;
 };
 
-type VideoOptional = {
-    title?: string;
-    author?: string;
-};
+type Animal = {
+    name: string;
+    age: number;
+}
 
-type VideoReadOnly = {
-    readonly title: string;
-    readonly author: string;
-};
+// mapped type은 js의 map과 비슷한 역할을 한다.
+// [1,2].map(item => item * item); // [1, 4]
+type Optional<T> = {
+    [P in keyof T]?: T[P]; // for...in
+}
+
+type ReadOnly<T> = {
+    readonly [P in keyof T]?: T[P]; // 한번 값을 정한 뒤로는 더 이상 바꿀 수 없다.
+}
+
+// 재사용성이 매우 좋다.
+type VideoOptional = Optional<Video>;
+type AnimalOptional = Optional<Animal>;
+
+// 사용 해보기
+let videoOp: VideoOptional;
+const value = {title: '123', created: '2020-02-03'};
+videoOp = value;
+
+// type VideoOptional = {
+//     title?: string;
+//     author?: string;
+// };
+
+// type VideoReadOnly = {
+//     readonly title: string;
+//     readonly author: string;
+// };
 
 
