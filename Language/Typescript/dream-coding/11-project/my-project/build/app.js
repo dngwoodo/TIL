02a099d67e7a5e60c5fb92f8ddd733e2b001e921
dngwoodo <dngwoodo@gmail.com>
2021-02-07 23:03:51 +0900

    Typescript/dream-coding: modal창 추가

--- Language/Typescript/dream-coding/11-project/my-project/build/app.js
+++ Language/Typescript/dream-coding/11-project/my-project/build/app.js
@@ -4,14 +4,41 @@ var videoButton = document.querySelector(".button--video");
 var noteButton = document.querySelector(".button--note");
 var taskButton = document.querySelector(".button--task");
 var cards = document.querySelector(".main__cards");
-// create
-imageButton.addEventListener("click", function () {
+var modalOveraly = document.querySelector(".modal__overlay");
+var modelCancelButton = document.querySelector(".modal .button--cancel");
+var modalForm = document.querySelector(".modal__form");
+// Create
+// 1. Image Button
+var hideModal = function () {
+    modalOveraly.classList.remove("hide");
+};
+var showModal = function () {
+    modalOveraly.classList.add("hide");
+};
+var createImageCard = function (title, url) {
     var li = document.createElement("li");
-    // 400px 이하일때 https://picsum.photos/400/300
-    // 400 ~ 700px일때 https://picsum.photos/700/300
-    // 701px ~ 일때
-    li.innerHTML = "\n    <li class=\"main__card\">\n      <img\n        srcset=\"https://picsum.photos/400/300 400w,\n                https://picsum.photos/700/300 700w\"\n        src=\"https://picsum.photos/1000/300\"\n        alt=\"image\"\n      />\n      <pre>Image Card</pre>\n      <button>\u274C</button>\n    </li>\n  ";
+    li.setAttribute("class", "main__card");
+    li.innerHTML = "\n      <img\n        src=\"" + url + "\"\n        alt=\"image\"\n      />\n      <pre>" + title + "</pre>\n      <button class=\"button button--cancel\">\u274C</button>\n  ";
     cards.appendChild(li);
+};
+imageButton.addEventListener("click", function () {
+    hideModal();
+});
+modelCancelButton.addEventListener("click", function () {
+    showModal();
+});
+modalForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var title = document.querySelector(".modal__title").value.trim();
+    var url = document.querySelector(".modal__url").value.trim();
+    if (title === "") {
+        alert("title은 필수입니다.");
+    }
+    if (url === "") {
+        alert("url은 필수입니다.");
+    }
+    createImageCard(title, url);
+    hideModal();
 });
 videoButton.addEventListener("click", function () { });
 noteButton.addEventListener("click", function () { });
--- Language/Typescript/dream-coding/11-project/my-project/src/app.ts
+++ Language/Typescript/dream-coding/11-project/my-project/src/app.ts
@@ -7,27 +7,67 @@ const videoButton = document.querySelector(
 const noteButton = document.querySelector(".button--note") as HTMLButtonElement;
 const taskButton = document.querySelector(".button--task") as HTMLButtonElement;
 const cards = document.querySelector(".main__cards") as HTMLUListElement;
+const modalOveraly = document.querySelector(
+  ".modal__overlay"
+) as HTMLDivElement;
+const modelCancelButton = document.querySelector(
+  ".modal .button--cancel"
+) as HTMLButtonElement;
 
-// create
-imageButton.addEventListener("click", () => {
+const modalForm = document.querySelector(".modal__form") as HTMLFormElement;
+
+// Create
+
+// 1. Image Button
+const hideModal = () => {
+  modalOveraly.classList.remove("hide");
+};
+
+const showModal = () => {
+  modalOveraly.classList.add("hide");
+};
+
+const createImageCard = (title: string, url: string) => {
   const li = document.createElement("li");
-  // 400px 이하일때 https://picsum.photos/400/300
-  // 400 ~ 700px일때 https://picsum.photos/700/300
-  // 701px ~ 일때
+  li.setAttribute("class", "main__card");
   li.innerHTML = `
-    <li class="main__card">
       <img
-        srcset="https://picsum.photos/400/300 400w,
-                https://picsum.photos/700/300 700w"
-        src="https://picsum.photos/1000/300"
+        src="${url}"
         alt="image"
       />
-      <pre>Image Card</pre>
-      <button>❌</button>
-    </li>
+      <pre>${title}</pre>
+      <button class="button button--cancel">❌</button>
   `;
   cards.appendChild(li);
+};
+
+imageButton.addEventListener("click", () => {
+  hideModal();
 });
+
+modelCancelButton.addEventListener("click", () => {
+  showModal();
+});
+
+modalForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = (document.querySelector(
+    ".modal__title"
+  ) as HTMLInputElement).value.trim();
+  const url = (document.querySelector(
+    ".modal__url"
+  ) as HTMLInputElement).value.trim();
+
+  if (title === "") {
+    alert("title은 필수입니다.");
+  }
+  if (url === "") {
+    alert("url은 필수입니다.");
+  }
+  createImageCard(title, url);
+  hideModal();
+});
+
 videoButton.addEventListener("click", () => {});
 noteButton.addEventListener("click", () => {});
 taskButton.addEventListener("click", () => {});
