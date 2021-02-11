49c1ad823cb4067c896edecd22c08125b4908676
dngwoodo <dngwoodo@gmail.com>
2021-02-11 20:03:59 +0900

    Typescript/dream-coding: 14.5 레이아웃과 첫번째 Page 컴포넌트 만들기 (Part2. 컴포넌트)💡

---
+++ Language/Typescript/dream-coding/11-project/lecture-project/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  ignorePatterns: [".eslintrc.js", "node_modules/"],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-non-null-assertion": false
+  },
+};
