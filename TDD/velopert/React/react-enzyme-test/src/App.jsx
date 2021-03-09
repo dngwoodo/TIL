2a3a11c09729ff198f9d9dafa242c7543b7d1c40
dngwoodo <dngwoodo@gmail.com>
2021-03-09 15:56:48 +0900

    TDD/velopert: React TDD 셋팅2

---
+++ TDD/velopert/React/react-enzyme-test/src/App.jsx
@@ -0,0 +1,12 @@
+import Profile from "./Profile";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile username="dngwoodo" name="김동우" />
+    </div>
+  );
+}
+
+export default App;
