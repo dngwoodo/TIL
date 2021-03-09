a837cf1fd9b6904d57e117c4b2f08c40139e962b
dngwoodo <dngwoodo@gmail.com>
2021-03-09 11:13:06 +0900

    TDD/velopert: @types/jest 설치 및 describe 를 사용해서 여러 테스트 케이스를 묶기

--- TDD/velopert/Javascript/sum.test.js
+++ TDD/velopert/Javascript/sum.test.js
@@ -1,6 +1,13 @@
-const sum = require("./sum");
+const { sum, sumOf } = require("./sum");
 
-// test or it을 사용가능. it을 사용하면 첫번째 인자를 영어로 작성 할 시 "말이 되게" 작성 가능함
-it("1 + 2 = 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("sum", () => {
+  // test or it을 사용가능. it을 사용하면 첫번째 인자를 영어로 작성 할 시 "말이 되게" 작성 가능함
+  it("1 + 2 = 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  it("calculates all numbers", () => {
+    const array = [1, 2, 3, 4, 5];
+    expect(sumOf(array)).toBe(15);
+  });
 });
