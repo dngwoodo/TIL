067366ef780f65093739b12f0fd3ea6435a475a1
dngwoodo <dngwoodo@gmail.com>
2021-03-09 14:59:46 +0900

    TDD/velopert: 실패 테스트 케이스 작성 && 테스트 케이스 성공시키기 - median

--- TDD/velopert/Javascript/stats.js
+++ TDD/velopert/Javascript/stats.js
@@ -12,3 +12,14 @@ exports.average = (numbers) => {
     return acc + current / length;
   }, 0);
 };
+
+// 숫자를 비교하기 위해선 comparFunction이 들어가야 한다.
+exports.sort = (numbers) => numbers.sort((a, b) => a - b);
+
+exports.median = (numbers) => {
+  const { length } = numbers; // 5
+  const middle = Math.floor(length / 2); // 2 <- 배열의 요소는 0
+  return length % 2
+    ? numbers[middle] // 홀수 일 때, numbers[2]
+    : (numbers[middle - 1] + numbers[middle]) / 2; // 짝수 일 때, (numbers[1] + numbers[2]) / 2
+};
--- TDD/velopert/Javascript/stats.test.js
+++ TDD/velopert/Javascript/stats.test.js
@@ -11,6 +11,23 @@ describe("stats", () => {
   });
 
   it("gets average value", () => {
-    expect(stats.average([1, 2, 3, 4])).toBe(2.5);
+    expect(stats.average([1, 2, 3])).toBe(2);
+  });
+
+  describe("medain", () => {
+    // 오름차순으로 정렬
+    it("sorts the array", () => {
+      expect(stats.sort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+    });
+
+    // 배열 길이 홀수 중앙 값 테스트
+    it("gets the median for odd length", () => {
+      expect(stats.median([1, 2, 3, 4, 5])).toBe(3);
+    });
+
+    // 배열 길이 짝수 중앙 값 테스트
+    it("gets the median for even length", () => {
+      expect(stats.median([1, 2, 3, 4, 5, 6])).toBe(3.5);
+    });
   });
 });
