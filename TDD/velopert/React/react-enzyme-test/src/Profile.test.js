c88b8023eda1e77be88cbb9d2b50aaa5f49d00d6
dngwoodo <dngwoodo@gmail.com>
2021-03-11 01:06:58 +0900

    TDD/velopert: props 접근

--- TDD/velopert/React/react-enzyme-test/src/Profile.test.js
+++ TDD/velopert/React/react-enzyme-test/src/Profile.test.js
@@ -7,4 +7,11 @@ describe("<Profile />", () => {
     const wrapper = mount(<Profile username="velopert" name="김민준" />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it("renders username and name", () => {
+    const wrapper = mount(<Profile username="velopert" name="김민준" />);
+    // 컴포넌트 인스턴스에 접근 가능함.
+    expect(wrapper.props().username).toBe("velopert");
+    expect(wrapper.props().name).toBe("김민준");
+  });
 });
