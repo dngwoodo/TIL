e3ebaa69db996c556f5df4d48626c6a0d3e70c57
dngwoodo <dngwoodo@gmail.com>
2021-03-12 10:46:11 +0900

    TDD/velopert: submit 이벤트 관리하기

--- TDD/velopert/React/rtl-tdd-todos/src/TodoForm.jsx
+++ TDD/velopert/React/rtl-tdd-todos/src/TodoForm.jsx
@@ -1,19 +1,29 @@
 import React, { useCallback, useState } from "react";
 
-const TodoForm = () => {
+const TodoForm = ({ onInsert }) => {
   const [value, setValue] = useState("");
   const onChange = useCallback((e) => {
     setValue(e.target.value);
   }, []);
 
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      onInsert(value); // 여기 테스팅
+      setValue(""); // 여기 테스팅
+    },
+    [onInsert, value]
+  );
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input
         type="text"
         placeholder="할 일을 입력해주세요"
         value={value}
         onChange={onChange}
       />
+      {/* 클릭 테스팅 */}
       <button type="submit">확인</button>
     </form>
   );
--- TDD/velopert/React/rtl-tdd-todos/src/TodoForm.spec.jsx
+++ TDD/velopert/React/rtl-tdd-todos/src/TodoForm.spec.jsx
@@ -22,4 +22,21 @@ describe("<TodoFrom />", () => {
     // expect(input.value).toBe(""); // 내가 생각 한것
     expect(input).toHaveAttribute("value", ""); // 블로그에서 정의한 것(내가 생각한 것 이랑 뭘 써도 상관없음), 해당 DOM에 특정 속성과 값을 있는지 확인해줌
   });
+
+  // click 되었을 경우 어떻게 동작하는 테스트 해보자(submit 이벤트 관리하기)
+  it("calls onInsert and clears input", () => {
+    const onInsert = jest.fn(); // mock function, 이걸 사용하면 toBeCalled, toBeCalledWith와 같은 것들을 사용해서 호출이 되었는지, 어떤 인자와 호출되었는지 확인 가능하다.
+    const utils = render(<TodoForm onInsert={onInsert}></TodoForm>);
+    const input = utils.getByPlaceholderText("할 일을 입력해주세요");
+    const button = utils.getByText("확인");
+    fireEvent.change(input, {
+      target: {
+        value: "TDD 배우기",
+      },
+    });
+    // 버튼 클릭
+    fireEvent.click(button);
+    expect(onInsert).toBeCalledWith("TDD 배우기"); // onInsert 가 'TDD 배우기'라는 파라미터와 함께 호출됐어야함
+    expect(input).toHaveAttribute("value", ""); // input이 비워져야함
+  });
 });
