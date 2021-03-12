2925b7a526229f830cd563626638ac77642828ec
dngwoodo <dngwoodo@gmail.com>
2021-03-12 17:20:57 +0900

    TDD/velopert: TodoApp 기본 할 일 항목 두개 보여주기

--- TDD/velopert/React/rtl-tdd-todos/src/TodoApp.spec.jsx
+++ TDD/velopert/React/rtl-tdd-todos/src/TodoApp.spec.jsx
@@ -9,4 +9,10 @@ describe("<TodoApp />", () => {
     getByText("확인"); // TodoForm 존재유무 확인
     getByTestId("TodoList"); // TodoList 존재유무 확인, data-testid를 의미한다.
   });
+
+  it("renders two defaults todos", () => {
+    const { getByText } = render(<TodoApp />);
+    getByText("TDD 배우기");
+    getByText("react-testing-library 사용하기");
+  });
 });
