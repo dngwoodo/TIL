1ee16a13d7f16337f14a267c691ec973d3d7ffc2
dngwoodo <dngwoodo@gmail.com>
2021-03-12 23:02:17 +0900

    TDD/velopert: 비동기적으로 바뀌는 컴포넌트 UI 테스트 - screen, findBy varient, getByRole 쿼리 사용해보기

--- TDD/velopert/React/rtl-tutorial/src/DelayedToggle.jsx
+++ TDD/velopert/React/rtl-tutorial/src/DelayedToggle.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback } from "react";
 
 const DelayedToggle = () => {
   const [toggle, setToggle] = useState(false);
   // 1초 후 toggle 값을 반전시키는 함수
   const onToggle = useCallback(() => {
     setTimeout(() => {
-      setToggle(toggle => !toggle);
+      setToggle((toggle) => !toggle);
     }, 1000);
   }, []);
   return (
     <div>
       <button onClick={onToggle}>토글</button>
       <div>
-        상태: <span>{toggle ? 'ON' : 'OFF'}</span>
+        상태: <span>{toggle ? "ON" : "OFF"}</span>
       </div>
       {toggle && <div>야호!!</div>}
     </div>
--- TDD/velopert/React/rtl-tutorial/src/DelayedToggle.spec.jsx
+++ TDD/velopert/React/rtl-tutorial/src/DelayedToggle.spec.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import DelayedToggle from "./DelayedToggle";
-import {
-  render,
-  fireEvent,
-  waitFor,
-  waitForElementToBeRemoved,
-} from "@testing-library/react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 
 describe("<DelayedToggle />", () => {
   it("reveals text when toggle is ON", async () => {
@@ -17,10 +12,11 @@ describe("<DelayedToggle />", () => {
   });
 
   it("toggles text ON/OFF", async () => {
-    const { getByText } = render(<DelayedToggle />);
-    const toggleButton = getByText("토글");
+    render(<DelayedToggle />);
+    // Role 종류는 https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques#roles <- 여기 참고
+    const toggleButton = screen.getByRole("button", { name: /토글/i }); // i는 대소문자 무시
     fireEvent.click(toggleButton);
-    const text = await waitFor(() => getByText("ON")); // 특정 엘리먼트가, 나타났거나, 바뀌었거나, 사라질때까지 대기(원래는 waitForElement 였음)
-    expect(text).toHaveTextContent("ON");
+    // screen.getByRole("") // <- 이렇게 일부러 오류를 내면 role을 쓸 수 있는 아이들이 출력 된다. 매우 편리하다. 보고 쓰면 된다. https://www.robinwieruch.de/react-testing-library <- 참고
+    await screen.findByText("야호!!"); // 돔이 바뀐 이후의 화면에서 찾고 싶으면 findBy를 이용한다.
   });
 });
