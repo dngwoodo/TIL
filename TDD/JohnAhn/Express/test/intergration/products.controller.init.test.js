28ab02c11752632a7a0e9d83d76d2e38f6f9eb85
dngwoodo <dngwoodo@gmail.com>
2021-03-13 16:14:03 +0900

    TDD/JohnAhn: createProduct 통합 테스트 작성3(에러처리)

--- TDD/JohnAhn/Express/test/intergration/products.controller.init.test.js
+++ TDD/JohnAhn/Express/test/intergration/products.controller.init.test.js
@@ -2,10 +2,18 @@ import request from "supertest";
 import app from "../../server";
 import newProduct from "../data/new-product.json";
 
-it("POST /api/products", async () => {
+it("POST /api/product", async () => {
   const response = await request(app).post("/api/product").send(newProduct);
   expect(response.statusCode).toBe(201);
   expect(response.body.name).toBe(newProduct.name);
   expect(response.body.description).toBe(newProduct.description);
   expect(response.body.price).toBe(newProduct.price);
+});
+
+it("should return 500 on POST /api/product", async () => {
+  const response = await request(app)
+    .post("/api/product")
+    .send({ name: "phone" });
+
+  expect(response.statusCode).toBe(500);
 });
