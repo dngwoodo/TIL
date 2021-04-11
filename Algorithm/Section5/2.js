c4e9ba71a1c5f90277dfb030fabf825fb37a8670
dngwoodo <dngwoodo@gmail.com>
2021-04-11 23:55:58 +0900

    Algorithm/Section5: 2. 공통원소 구하기

--- Algorithm/Section5/2.js
+++ Algorithm/Section5/2.js
@@ -4,8 +4,10 @@
 
 // ▣ 입력설명
 // 첫 번째 줄에 집합 A의 크기 N(1<=N<=30,000)이 주어집니다.
-// 두 번째 줄에 N개의 원소가 주어집니다. 원소가 중복되어 주어지지 않습니다. 세 번째 줄에 집합 B의 크기 M(1<=M<=30,000)이 주어집니다.
-// 네 번째 줄에 M개의 원소가 주어집니다. 원소가 중복되어 주어지지 않습니다. 각 집합의 원소는 1,000,000,000이하의 자연수입니다.
+// 두 번째 줄에 N개의 원소가 주어집니다. 원소가 중복되어 주어지지 않습니다.
+// 세 번째 줄에 집합 B의 크기 M(1<=M<=30,000)이 주어집니다.
+// 네 번째 줄에 M개의 원소가 주어집니다. 원소가 중복되어 주어지지 않습니다.
+// 각 집합의 원소는 1,000,000,000이하의 자연수입니다.
 
 // ▣ 출력설명
 // 두 집합의 공통원소를 오름차순 정렬하여 출력합니다.
@@ -19,9 +21,37 @@
 // ▣ 출력예제 1
 // 2 3 5
 
+// 푼시간: 1분
+
+// 내 풀이
+// function solution(arr1, arr2) {
+//   let answer = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr1.length; j++) {
+//       if (arr1[i] === arr2[j]) answer.push(arr1[i]);
+//     }
+//   }
+//   answer.sort((a, b) => a - b);
+//   return answer;
+// }
+
+// 강의 풀이
+let a = [1, 3, 9, 5, 2];
+let b = [3, 2, 5, 7, 8];
+console.log(solution(a, b));
+
 function solution(arr1, arr2) {
   let answer = [];
-
+  arr1.sort();
+  arr2.sort();
+  let p1 = (p2 = 0); // p1, p2를 포인터로 활용
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] == arr2[p2]) {
+      answer.push(arr1[p1++]);
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) p1++;
+    else p2++;
+  }
   return answer;
 }
 
