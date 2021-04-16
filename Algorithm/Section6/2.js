9e948235fb56c341ac1e4a8c7103bef31bbb3af2
dngwoodo <dngwoodo@gmail.com>
2021-04-17 00:54:13 +0900

    Algorithm/Section6: 2. 괄호문자제거

--- Algorithm/Section6/2.js
+++ Algorithm/Section6/2.js
@@ -8,16 +8,25 @@
 // ▣ 출력설명
 // 남은 문자만 출력한다.
 
-// ▣ 입력예제 1 
+// ▣ 입력예제 1
 // (A(BC)D)EF(G(H)(IJ)K)LM(N)
-// ▣ 출력예제 1 
+// ▣ 출력예제 1
 // EFLM
 
-function solution(s){  
-  let answer;
+// 푼 시간: 20분
 
+function solution(s) {
+  let answer;
+  let stack = [];
+  for (let x of s) {
+    if (x === ")") {
+      // NOTE: ( 까지 다 빼버림
+      while (stack.pop() !== "(");
+    } else stack.push(x);
+  }
+  answer = stack.join("");
   return answer;
 }
 
-let str="(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
 console.log(solution(str));
