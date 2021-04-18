f2dfe0e3444912619b1ce496275079149b84aefb
dngwoodo <dngwoodo@gmail.com>
2021-04-19 04:24:42 +0900

    Algorithm/Section6: 4. 후위식 연산(postfix)

--- Algorithm/Section6/4.js
+++ Algorithm/Section6/4.js
@@ -15,9 +15,22 @@
 // ▣ 출력예제 1 
 // 12
 
+// 푼 시간: 30분
 function solution(s){  
   let answer;
-
+  let stack=[];
+  for(let x of s){
+      if(!isNaN(x)) stack.push(Number(x));
+      else{
+          let rt=stack.pop();
+          let lt=stack.pop();
+          if(x==='+') stack.push(lt+rt);
+          else if(x==='-') stack.push(lt-rt);
+          else if(x==='*') stack.push(lt*rt);
+          else if(x==='/') stack.push(lt/rt);
+      }
+  }
+  answer=stack[0];
   return answer;
 }
 
