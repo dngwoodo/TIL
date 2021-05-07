644567977b698c4c3f6364e646e1c552099a0fb9
dngwoodo <dngwoodo@gmail.com>
2021-05-07 09:27:56 +0900

    Algorithm/Section8: 4. 부분집합 구하기(DFS)

--- Algorithm/Section8/4.js
+++ Algorithm/Section8/4.js
@@ -12,8 +12,7 @@
 // 3
 
 // ▣ 출력예제 1
-// 123
-
+// 1 2 3
 // 1 2
 // 1 3
 // 1
@@ -21,9 +20,34 @@
 // 2
 // 3
 
+// 푼 시간: 25분, GG
+
+// 강의풀이
+// NOTE: 왼쪽 간선은 부분집합에 포함한다, 오른쪽 간선은 부분집합에 포함하지 않는다로 정하고 이진트리를 그린다.
 function solution(n) {
   let answer = [];
+  let ch = Array.from({ length: n + 1 }, () => 0); // NOTE: n + 1개 만큼 요소 가진 배열 생성
+  function DFS(v) {
+    // NOTE: DFS(4)에서 멈추고 answer에 답 대입
+    if (v === n + 1) {
+      let tmp = "";
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 1) tmp += i + " ";
+      }
+      // NOTE: 공집합("") 를 빼기 위한 조건
+      if (tmp.length > 0) answer.push(tmp.trim());
+    } else {
+      // NOTE: 왼쪽 노드
+      ch[v] = 1; // NOTE: 포함을 의미
+      DFS(v + 1); // NOTE: 왼쪽 노드 값을 의미
 
+      // NOTE: 오른쪽 노드
+      ch[v] = 0; // NOTE: 미포함을 의미
+      DFS(v + 1); // NOTE: 오른쪽 노드 값을 의미
+    }
+  }
+
+  DFS(1);
   return answer;
 }
 
