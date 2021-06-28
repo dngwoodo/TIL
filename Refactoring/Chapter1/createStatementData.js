33038a29a49f4b61612a345a9934acd172768ee9
dngwoodo <dngwoodo@gmail.com>
2021-06-28 14:35:54 +0900

    1.8 다형성을 활용해 계산 코드 재구성하기: 생성자를 팩터리 함수로 바꾸기

--- Refactoring/Chapter1/createStatementData.js
+++ Refactoring/Chapter1/createStatementData.js
@@ -1,3 +1,14 @@
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
+}
+
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
@@ -42,6 +53,9 @@ class PerformanceCalculator {
   }
 }
 
+class TragedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {}
+
 module.exports = function createStatementData(invoice, plays) {
   const statementData = {};
 
@@ -53,7 +67,7 @@ module.exports = function createStatementData(invoice, plays) {
   return statementData;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(
+    const calculator = createPerformanceCalculator(
       aPerformance,
       playFor(aPerformance)
     );
