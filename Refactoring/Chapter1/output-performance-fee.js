4994473584e4fd3d2aab092da2208aefe60ced28
dngwoodo <dngwoodo@gmail.com>
2021-06-28 14:12:38 +0900

    1.7 중간점검: 두파일(과 두단계)로 분리됨

--- Refactoring/Chapter1/output-performance-fee.js
+++ Refactoring/Chapter1/output-performance-fee.js
@@ -19,6 +19,27 @@ function renderPlainText(data) {
 
   return result;
 
+  function htmlStatement(invoice, plays) {
+    return renderHTML(createStatementData(invoice, plays));
+  }
+
+  function renderHTML(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+
+    for (let perf of data.performances) {
+      result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+      result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em>credits</p>`;
+
+    return result;
+  }
+
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
