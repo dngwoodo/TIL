ad61feb9570cbe105503826b387144d0497336cc
dngwoodo <dngwoodo@gmail.com>
2021-06-28 10:20:34 +0900

    1-4 statement() 함수쪼개기: 함수 선언 바꾸기
    play는 aPerformance에서 얻어올 수 있으므로 매개변수로 전달할 필요가
    없다. 그렇기 때문에 play 매개변수를 없애기 위한 작업이다. p37

--- Refactoring/Chapter1/output-performance-fee.js
+++ Refactoring/Chapter1/output-performance-fee.js
@@ -1,30 +1,6 @@
 const invoices = require("./invoices.json");
 const plays = require("./plays.json");
 
-function amountFor(aPerformance, play) {
-  let result = 0;
-
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
-  }
-
-  return result;
-}
-
 function statement(invoice, plays) {
   let totalAmount = 0; // 전체 공연료
   let volumeCredits = 0; // 포인트
@@ -39,8 +15,32 @@ function statement(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+
+    return result;
+  }
+
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
