102511b64667e7abe757cc41628538f9ba1c9907
dngwoodo <dngwoodo@gmail.com>
2021-06-28 13:08:55 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기: amountFor 함수 이동
    중간데이터에 넣기위해서 amountFor 함수를 statement로 옮김

--- Refactoring/Chapter1/output-performance-fee.js
+++ Refactoring/Chapter1/output-performance-fee.js
@@ -10,11 +10,36 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance); // 얕은 복사
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${aPerformance.result.type}`);
+    }
+
+    return result;
   }
 }
 
@@ -23,9 +48,7 @@ function renderPlainText(data) {
 
   for (let perf of data.performances) {
     // 청구 내역을 출력한다
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
 
   result += `총액: ${usd(totalAmount())}\n`;
@@ -37,7 +60,7 @@ function renderPlainText(data) {
     let result = 0;
 
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
 
     return result;
@@ -70,30 +93,6 @@ function renderPlainText(data) {
     // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.result.type}`);
-    }
 
     return result;
   }
