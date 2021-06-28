cd32f0233901a6640610c286764419dad1ec41e8
dngwoodo <dngwoodo@gmail.com>
2021-06-28 13:58:48 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기: 모듈화

--- Refactoring/Chapter1/output-performance-fee.js
+++ Refactoring/Chapter1/output-performance-fee.js
@@ -1,76 +1,9 @@
 const invoices = require("./invoices.json");
 const plays = require("./plays.json");
+const createStatementData = require("./createStatementData.js");
 
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
-}
-
-function createStatementData(invoice, plays) {
-  const statementData = {};
-
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance); // 저자는 최대한 데이터를 불변으로 사용
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-
-  return statementData;
-
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance); // 얕은 복사
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.result.type}`);
-    }
-
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0; // 포인트
-
-    // 포인트를 적립한다.
-    result += Math.max(aPerformance.audience - 30, 0);
-
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-  }
-
-  function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
-
-  function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
 }
 
 function renderPlainText(data) {
