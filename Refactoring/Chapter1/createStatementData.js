620c24a757b424bf70cf0fe3d2cdc7182864f60d
dngwoodo <dngwoodo@gmail.com>
2021-06-28 14:25:22 +0900

    1.8 다형성을 활용해 계산 코드 재구성하기: 함수 옮기기, 함수를 인라인
    amountFor을 PerformanceCalculator 클래스로 이동

--- Refactoring/Chapter1/createStatementData.js
+++ Refactoring/Chapter1/createStatementData.js
@@ -1,7 +1,31 @@
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
-    this.performances = aPerformance;
+    this.performance = aPerformance;
     this.play = aPlay;
+  }
+
+  get amount() {
+    let result = 0;
+
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.performance.result.type}`);
+    }
+
+    return result;
   }
 }
 
@@ -23,7 +47,7 @@ module.exports = function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance); // 얕은 복사
 
     result.play = playFor(result);
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
 
     return result;
@@ -31,30 +55,6 @@ module.exports = function createStatementData(invoice, plays) {
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.result.type}`);
-    }
-
-    return result;
   }
 
   function volumeCreditsFor(aPerformance) {
