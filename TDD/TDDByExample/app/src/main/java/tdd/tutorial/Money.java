3101167e84f6b8e078a3877baf990881d03a9981
dngwoodo <dngwoodo@gmail.com>
2021-12-28 23:40:08 +0900

    5일차(8장)

--- TDD/TDDByExample/app/src/main/java/tdd/tutorial/Money.java
+++ TDD/TDDByExample/app/src/main/java/tdd/tutorial/Money.java
@@ -3,6 +3,18 @@ package tdd.tutorial;
 public class Money {
     protected int amount;
 
+    // 외부에서 franc, dollar 생성자를 직접 사용하는 경우를 없애기 위해서.
+    // 외부 참조를 없앴다라고 한다. 이렇게 하면 Money라는 곳에 공통적으로 묶기 쉬워진다.
+    // 팩토리 메소드
+    public static Dollar dollar(int number) {
+        return new Dollar(number);
+    }
+
+    // 팩토리 메소드
+    public static Franc franc(int number) {
+        return new Franc(number);
+    }
+
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
@@ -13,3 +25,4 @@ public class Money {
         return getClass().equals(money.getClass()) && amount == money.amount;
     }
 }
+
